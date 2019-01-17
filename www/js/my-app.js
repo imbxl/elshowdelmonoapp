3271bc0ba9755ee3f01c43bd6fe0bbd3e21d3f9f
imbxl <imbxl@hotmail.com>
2019-01-17 06:13:10 -0300

    se

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -45,20 +45,15 @@ function CloseAuditoria(){
 }
 
 function SacarFoto(){	
-	navigator.mediaDevices.getUserMedia({
-		'audio': true,
-		'video': {
-			facingMode: 'user'
-		}
-	}).then(function(getmedia) {
-		var track = getmedia.getVideoTracks()[0];
-		var imageCapture = new ImageCapture(track);
-		imageCapture.takePhoto().then(blob => {
-			console.log('Photo taken: ' + blob.type + ', ' + blob.size + 'B');
-			const image = document.getElementById('fotopreview') // img is an <img> tag
-			image.src = URL.createObjectURL(blob);
-		})
-		.catch(err => console.error('takePhoto() failed: ', err));
+	navigator.camera.getPicture(function(imageData){
+	   var image = document.getElementById('fotopreview');
+	   image.src = "data:image/jpeg;base64," + imageData;
+	}, function(fail){
+		alert(fail);
+	},	{
+	  quality: 50,
+	  destinationType: destinationType.FILE_URI,
+	  saveToPhotoAlbum: true
 	});
 }
 $$(document).on('deviceready', function() {	  
