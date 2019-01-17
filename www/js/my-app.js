1562146e27e2bb92de6944a50d50f0cfe45fceba
imbxl <imbxl@hotmail.com>
2019-01-17 06:46:46 -0300

    se

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -45,30 +45,14 @@ function CloseAuditoria(){
 }
 
 
-function onPhotoDataSuccess(imageData) {
-  var smallImage = document.getElementById('fotopreview');
-  smallImage.style.display = 'block';
-  smallImage.src = "data:image/jpeg;base64," + imageData;
-}
-
-function onPhotoURISuccess(imageURI) {
-  var largeImage = document.getElementById('fotopreview');
-  largeImage.style.display = 'block';
-  largeImage.src = imageURI;
-}
-function capturePhoto() {
-  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
-	destinationType: destinationType.DATA_URL });
-}
-function capturePhotoEdit() {
-  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
-	destinationType: destinationType.DATA_URL });
-}
-function getPhoto(source) {
-  navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,	destinationType: destinationType.FILE_URI,	sourceType: source });
-}
-function onFail(message) {
-  alert('Failed because: ' + message);
+function FotoItem(id) {
+  	navigator.camera.getPicture(function(imageData) {
+	  	/*var smallImage = document.getElementById('fotopreview');
+	  	smallImage.style.display = 'block';
+	  	smallImage.src = "data:image/jpeg;base64," + imageData;*/
+		$$('.audi_item_'+id+' .img').val("data:image/jpeg;base64,"+imageData);
+		$$('.audi_item_'+id+' .foto').addClass('ok');
+	}, function(){}, { quality: 50,	destinationType: destinationType.DATA_URL });
 }
 
 var pictureSource;
